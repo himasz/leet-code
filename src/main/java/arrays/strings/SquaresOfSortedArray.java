92ec7e09cbfdef8ef77070277e3fb59010628bb1
ebrahimzidan <himaszidan@gmail.com>
2024-07-15 14:45:10 +0200

    add SquaresOfSortedArray.java

---
+++ src/main/java/arrays/strings/SquaresOfSortedArray.java
@@ -0,0 +1,31 @@
+package arrays.strings;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        int k = nums.length - 1;
+        int[] results = new int[nums.length];
+        while (j >= i) {
+            int ith = nums[i];
+            int jth = nums[j];
+            if (ith * ith > jth * jth) {
+                results[k] = ith * ith;
+                i++;
+            } else {
+                results[k] = jth * jth;
+                j--;
+            }
+            k--;
+        }
+        return results;
+    }
+
+    public static void main(String[] args) {
+        int[] s = {-7, -3, 2, 3, 11};
+        System.out.println(Arrays.toString(s));
+        System.out.println(Arrays.toString(new SquaresOfSortedArray().sortedSquares(s)));
+    }
+}
